250480c7001905d7bfd88225949d0c97d56979fe
Liudmyla <hrytsiuk.liudmyla@gmail.com>
2021-05-26 08:59:23 +0300

    Modified file

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,13 @@
 class ItemsController < ApplicationController
+  before_action :get_list
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def new
-    @list = List.find(params[:list_id])
-    @list.items << Item.new
+    @item = @list.items.new
   end
 
   def index
-    @list = List.find(params[:list_id])
-    @list.items
+    @items = @list.items
   end
 
   def show
@@ -18,21 +19,51 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @list = List.find(params[:list_id])
-    @item = @list.items.create(item_params)
-    redirect_to list_path(@list)
+    @item = @list.items.build(item_params)
+
+    respond_to do |format|
+     if @item.save
+        format.html { redirect_to list_items_path(@list), notice: 'item was successfully created.' }
+        format.json { render :show, status: :created, location: @item }
+     else
+        format.html { render :new }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @item.update(item_params)
+        format.html { redirect_to list_item_path(@list), notice: 'Item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @item }
+      else
+        format.html { render :edit }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
-    @list = List.find(params[:list_id])
-    @item = @list.items.find_by(uid: item_params[:uid])
     @item.destroy
-    redirect_to list_items_path(@list)
+
+    respond_to do |format|
+      format.html { redirect_to list_items_path(@list), notice: 'Item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
 
+  def get_list
+    @list = List.find(params[:list_id])
+  end
+
+  def set_item
+    @item = @list.items.find_by(uid: params[:uid])
+  end
+
   def item_params
-    (params).permit(:uid, :name, :price, :list_id)
+    params.permit(:uid, :name, :price, :list_id)
   end
 end
